a5c6a3322f98c4bf988d7eae1b20a9df1eaa31e3
Jeswinkuttiyani <118618288+Jeswinkuttiyani@users.noreply.github.com>
2024-07-29 12:19:08 +0530

    Add files via upload

--- cpumenu.c
+++ cpumenu.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main(){
-int ch,cho;
+int ch=0,cho;
 while(ch==0){
 printf("Enter the choice: 1)FCFS\t2)SJF\t3)priority\t4)RR\t5)exit\n");
 scanf("%d",&cho);
 switch (cho){
-case 1:
-   int n,i;{
+case 1:{
+   int n,i;
    float twt=0,ttat=0;
    printf("enter the no of process:\n");
    scanf("%d",&n);
@@ -146,8 +146,72 @@ case 3:{
    printf("tot wt= %f\ntot tat= %f\n",twt,ttat);
    break;
    }
+case 4:
+   {
+   int tot=0,lim,tq,flag=0,i;
+   printf("Enter the number of process and time quantum:\n");
+   scanf("%d%d",&lim,&tq);
+   int bt[lim],pid[lim],rt[lim],tat[lim],wt[lim],prt[30],f=1;
+   float avg_wt=0,avg_tat=0;
+   printf("Enter the process id and brust time of process: \n");
+   for(i=0;i<lim;i++){
+   scanf("%d%d",&pid[i],&bt[i]);
+   rt[i]=bt[i];
+   }
+   prt[0]=0;
+   printf("\n");
+   printf("GHANT CHART\n");
+   while(flag!=lim){
+   for(i=0;i<lim;i++){
+   if(rt[i]>tq){
+     printf("   p%d\t",pid[i]);
+     tot=tot+tq;
+     rt[i]=rt[i]-tq;
+     prt[f]=tot;
+     f++;
+     }
+   else if(rt[i]==0){
+   
+     continue;
+     }
+   else{
+   printf("    p%d\t",pid[i]);
+     tot+=rt[i];
+     rt[i]=0;
+     tat[i]=tot;
+     wt[i]=tot-bt[i];
+     prt[f]=tot;
+     flag++;
+     f++;
+     }
+   }
+   }
+   for(i=0;i<lim;i++)
+  {
+  avg_tat+=tat[i];
+  avg_wt+=wt[i];
+  }
+  printf("\n");
+  for(i=0;i<f;i++)
+  {
+  printf("%d\t",prt[i]);
+   }
+  printf("\n");
+  printf("\n");
+  printf(" pid\tbt\ttat\twt\n");
+  for(i=0;i<lim;i++){
+  printf(" %d\t%d\t%d\t%d\n",pid[i],bt[i],tat[i],wt[i]);
+  }
+  printf("total wt=%f\n",avg_wt);
+    printf("total tat=%f\n",avg_tat);
+  printf("Average wt=%f\n",avg_wt/lim);
+  printf("Average tat=%f\n",avg_tat/lim);
+  break;
+  }
+  
 case 5:{
- break;
+   ch=1;
+   break;
  }
  }
  }
